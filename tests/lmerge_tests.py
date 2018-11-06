fb2481d785710bf7e2986eebc7a623acecbaf659
Dave Larson <delarson@wustl.edu>
2018-11-06 22:41:49 +0000

    Add in beginnings of tests for simple field inference

--- tests/lmerge_tests.py
+++ tests/lmerge_tests.py
@@ -56,6 +56,50 @@ class LmergeIntegrationTest(TestCase):
                 self.assertFalse(result)
         os.remove(temp_output_path)
 
+    def run_integration_test_for_field_inference(self):
+        test_directory = os.path.dirname(os.path.abspath(__file__))
+        self.test_data_dir = os.path.join(test_directory, 'test_data', 'lmerge')
+        input_file = os.path.join(self.test_data_dir, 'field_inference.present.vcf')
+        expected_result = os.path.join(self.test_data_dir, 'field_inference.expected.vcf')
+        temp_descriptor, temp_output_path = tempfile.mkstemp(suffix='.vcf')
+        with os.fdopen(temp_descriptor, 'w') as output_handle:
+            # FIXME this is pretty hacky
+            temp_handle, sys.stdout = sys.stdout, output_handle
+
+            lmerge.l_cluster_by_line(input_file, 0.0, 20, True, True)
+            output_handle.flush()
+            expected_lines = open(expected_result).readlines()
+            produced_lines = open(temp_output_path).readlines()
+            diff = difflib.unified_diff(produced_lines, expected_lines, fromfile=temp_output_path, tofile=expected_result)
+            result = ''.join(diff)
+            if result != '':
+                for line in result:
+                    sys.stdout.write(line)
+                self.assertFalse(result)
+        os.remove(temp_output_path)
+
+    def run_integration_test_for_field_inference_no_fields(self):
+        test_directory = os.path.dirname(os.path.abspath(__file__))
+        self.test_data_dir = os.path.join(test_directory, 'test_data', 'lmerge')
+        input_file = os.path.join(self.test_data_dir, 'field_inference.all_absent.vcf')
+        expected_result = os.path.join(self.test_data_dir, 'field_inference.expected.vcf')
+        temp_descriptor, temp_output_path = tempfile.mkstemp(suffix='.vcf')
+        with os.fdopen(temp_descriptor, 'w') as output_handle:
+            # FIXME this is pretty hacky
+            temp_handle, sys.stdout = sys.stdout, output_handle
+
+            lmerge.l_cluster_by_line(input_file, 0.0, 20, True, True)
+            output_handle.flush()
+            expected_lines = open(expected_result).readlines()
+            produced_lines = open(temp_output_path).readlines()
+            diff = difflib.unified_diff(produced_lines, expected_lines, fromfile=temp_output_path, tofile=expected_result)
+            result = ''.join(diff)
+            if result != '':
+                for line in result:
+                    sys.stdout.write(line)
+                self.assertFalse(result)
+        os.remove(temp_output_path)
+
 
 if __name__ == "__main__":
     main()
